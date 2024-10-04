544d77013e41369e9a67a386bdf1ff3fe71071ae
Muhammad Farabie <farabie024@gmail.com>
2024-10-04 14:12:49 +0700

    Validasi kalau misalkan dms_id nya sudah ada di installation record maka tidak usah ditampilkan

--- lib/presentation/pages/dashboard_page.dart
+++ lib/presentation/pages/dashboard_page.dart
@@ -18,6 +18,9 @@ class _DashboardPageState extends State<DashboardPage> {
     super.initState();
     context.read<TicketByUserBloc>().add(FetchTicketByUserCM());
     context.read<TicketByUserBloc>().add(FetchTicketByUserPM());
+    context
+        .read<InstallationRecordsUsernameBloc>()
+        .add(FetchInstallationRecordsUsername('spvcentral.1'));
   }
 
   @override
@@ -131,78 +134,229 @@ class _DashboardPageState extends State<DashboardPage> {
         ),
         const Gap(20),
         BlocBuilder<TicketByUserBloc, TicketByUserState>(
-            builder: (context, state) {
-          int cmCount = 0;
-          int pmCount = 0;
-          int totalTicketsInstallation = 0;
-          // ticketByUserCM = [];
-          // ticketByUserPM = [];
+          builder: (context, ticketState) {
+            return BlocBuilder<InstallationRecordsUsernameBloc,
+                InstallationRecordsUsernameState>(
+              builder: (context, installationState) {
+                int cmCount = 0;
+                int pmCount = 0;
+                int totalTicketsInstallation = 0;
 
-          if (state is TicketByUserLoaded) {
-            cmCount = state.cmTickets.length;
-            pmCount = state.pmTickets.length;
-            totalTicketsInstallation = cmCount + pmCount;
-          }
-          return GridView.count(
-            padding: const EdgeInsets.all(0),
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            crossAxisCount: 2,
-            childAspectRatio: 1.5,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 16,
-            children: [
-              buildItemModuleMenu(
-                  asset: 'assets/images/inspection_bg.png',
-                  status: 'Inspection',
-                  total: 1,
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const DashboardPage()));
-                  }),
-              buildItemModuleMenu(
-                asset: 'assets/images/installation_bg.png',
-                status: 'Installation',
-                total: totalTicketsInstallation,
-                onTap: () {
-                  // Trigger the fetch events
-                  context.read<TicketByUserBloc>().add(FetchTicketByUserCM());
-                  context.read<TicketByUserBloc>().add(FetchTicketByUserPM());
+                List<TicketByUser> filteredCmTickets = [];
+                List<TicketByUser> filteredPmTickets = [];
 
-                  // Set the flag to true to navigate when data is loaded
-                  setState(() {
-                    navigateToInstallation = true;
-                  });
-                },
-              ),
-              buildItemModuleMenu(
-                  asset: 'assets/images/rectification_bg.png',
-                  status: 'Rectification',
-                  total: 1,
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const DashboardPage()));
-                  }),
-              buildItemModuleMenu(
-                  asset: 'assets/images/qualityaudit_bg.png',
-                  status: 'Quality Audit',
-                  total: 1,
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const DashboardPage()));
-                  }),
-            ],
-          );
-        })
+                // Check if the state from TicketByUserBloc is loaded
+                if (ticketState is TicketByUserLoaded) {
+                  // Initialize ticket lists
+                  List<TicketByUser> cmTickets = ticketState.cmTickets;
+                  List<TicketByUser> pmTickets = ticketState.pmTickets;
+
+                  // Check if InstallationRecordsUsernameState is loaded or empty
+                  if (installationState is InstallationRecordsUsernameLoaded &&
+                      installationState.records.isNotEmpty) {
+                    final installedTicketNumbers = installationState.records
+                        .map((record) => record.dmsId)
+                        .toSet();
+
+                    // Filter out CM and PM tickets that already exist in installation records
+                    filteredCmTickets = cmTickets
+                        .where((ticket) => !installedTicketNumbers
+                            .contains(ticket.ticketNumber))
+                        .toList();
+
+                    filteredPmTickets = pmTickets
+                        .where((ticket) => !installedTicketNumbers
+                            .contains(ticket.ticketNumber))
+                        .toList();
+                  } else {
+                    // If no installation records exist, use all tickets
+                    filteredCmTickets = cmTickets;
+                    filteredPmTickets = pmTickets;
+                  }
+
+                  // Update counts based on filtered tickets
+                  cmCount = filteredCmTickets.length;
+                  pmCount = filteredPmTickets.length;
+                  totalTicketsInstallation = cmCount + pmCount;
+                }
+
+                return GridView.count(
+                  padding: const EdgeInsets.all(0),
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.5,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  children: [
+                    buildItemModuleMenu(
+                      asset: 'assets/images/inspection_bg.png',
+                      status: 'Inspection',
+                      total: 1,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const DashboardPage(),
+                          ),
+                        );
+                      },
+                    ),
+                    buildItemModuleMenu(
+                      asset: 'assets/images/installation_bg.png',
+                      status: 'Installation',
+                      total: totalTicketsInstallation,
+                      onTap: () {
+                        if (totalTicketsInstallation > 0) {
+                          // Use filtered tickets for the navigation
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ListInstallationPage(
+                                cmCount: filteredCmTickets.length,
+                                pmCount: filteredPmTickets.length,
+                                ticketByUserCM: filteredCmTickets,
+                                ticketByUserPM: filteredPmTickets,
+                              ),
+                            ),
+                          );
+
+                          // Reset navigation flag if required
+                          setState(() {
+                            navigateToInstallation = false;
+                          });
+                        } else {
+                          // Show a message if no tickets are available after filtering
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                'No tickets available for installation.',
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                    buildItemModuleMenu(
+                      asset: 'assets/images/rectification_bg.png',
+                      status: 'Rectification',
+                      total: 1,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const DashboardPage(),
+                          ),
+                        );
+                      },
+                    ),
+                    buildItemModuleMenu(
+                      asset: 'assets/images/qualityaudit_bg.png',
+                      status: 'Quality Audit',
+                      total: 1,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const DashboardPage(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
       ],
     );
   }
+
+  // Widget buildQmsModule() {
+  //   return Column(
+  //     crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: [
+  //       const Text(
+  //         'Module',
+  //         style: TextStyle(
+  //           fontSize: 20,
+  //           color: Colors.black,
+  //           fontWeight: FontWeight.w600,
+  //         ),
+  //       ),
+  //       const Gap(20),
+  //       BlocBuilder<TicketByUserBloc, TicketByUserState>(
+  //           builder: (context, state) {
+  //         int cmCount = 0;
+  //         int pmCount = 0;
+  //         int totalTicketsInstallation = 0;
+
+  //         if (state is TicketByUserLoaded) {
+  //           cmCount = state.cmTickets.length;
+  //           pmCount = state.pmTickets.length;
+  //           totalTicketsInstallation = cmCount + pmCount;
+  //         }
+  //         return GridView.count(
+  //           padding: const EdgeInsets.all(0),
+  //           physics: const NeverScrollableScrollPhysics(),
+  //           shrinkWrap: true,
+  //           crossAxisCount: 2,
+  //           childAspectRatio: 1.5,
+  //           mainAxisSpacing: 16,
+  //           crossAxisSpacing: 16,
+  //           children: [
+  //             buildItemModuleMenu(
+  //                 asset: 'assets/images/inspection_bg.png',
+  //                 status: 'Inspection',
+  //                 total: 1,
+  //                 onTap: () {
+  //                   Navigator.push(
+  //                       context,
+  //                       MaterialPageRoute(
+  //                           builder: (context) => const DashboardPage()));
+  //                 }),
+  //             buildItemModuleMenu(
+  //               asset: 'assets/images/installation_bg.png',
+  //               status: 'Installation',
+  //               total: totalTicketsInstallation,
+  //               onTap: () {
+  //                 // Trigger the fetch events
+  //                 context.read<TicketByUserBloc>().add(FetchTicketByUserCM());
+  //                 context.read<TicketByUserBloc>().add(FetchTicketByUserPM());
+
+  //                 // Set the flag to true to navigate when data is loaded
+  //                 setState(() {
+  //                   navigateToInstallation = true;
+  //                 });
+  //               },
+  //             ),
+  //             buildItemModuleMenu(
+  //                 asset: 'assets/images/rectification_bg.png',
+  //                 status: 'Rectification',
+  //                 total: 1,
+  //                 onTap: () {
+  //                   Navigator.push(
+  //                       context,
+  //                       MaterialPageRoute(
+  //                           builder: (context) => const DashboardPage()));
+  //                 }),
+  //             buildItemModuleMenu(
+  //                 asset: 'assets/images/qualityaudit_bg.png',
+  //                 status: 'Quality Audit',
+  //                 total: 1,
+  //                 onTap: () {
+  //                   Navigator.push(
+  //                       context,
+  //                       MaterialPageRoute(
+  //                           builder: (context) => const DashboardPage()));
+  //                 }),
+  //           ],
+  //         );
+  //       })
+  //     ],
+  //   );
+  // }
 
   Widget buildProgressQmsTicket() {
     return Column(
